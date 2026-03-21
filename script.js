70c7ee3d94a899df86d7619843f556cd97bf8cbf
thepoorbulliedlion <82186215+thepoorbulliedlion@users.noreply.github.com>
2026-03-21 16:42:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,81 @@
-    let character = {
-      p: [100, 100],
-      v: [0,0],
-      element: document.getElementById("character")
-    };
+let character = {
+    p: [100, 100],
+    v: [0,0],
+    element: document.getElementById("character")
+};
 
-    let keysDown = {
-      left: false, 
-      right: false, 
-      up: false, 
-      down: false
-    }
+let keysDown = {
+    left: false, 
+    right: false, 
+    up: false, 
+    down: false
+}
 
-    function recordKeyDown(e) {
-      if (e.code == "ArrowDown") {keysDown.down = true;}
-      if (e.code == "ArrowUp") {keysDown.up = true;}
-      if (e.code == "ArrowRight") {keysDown.right = true;}
-      if (e.code == "ArrowLeft") {keysDown.left = true;}
-    }
-    addEventListener("keydown", recordKeyDown)
+function recordKeyDown(e) {
+  switch (e.code) {
+    case "ArrowDown":
+    case "KeyS":
+      keysDown.down = true;
+      break;
 
-    function recordKeyUp(e) {
-      if (e.code == "ArrowDown") {keysDown.down = false;}
-      if (e.code == "ArrowUp") {keysDown.up = false;}
-      if (e.code == "ArrowRight") {keysDown.right = false;}
-      if (e.code == "ArrowLeft") {keysDown.left = false;}
-    }
-    addEventListener("keyup", recordKeyUp)
+    case "ArrowUp":
+    case "KeyW":
+      keysDown.up = true;
+      break;
 
-    let speed = 2;
-    function draw() {
-      if (keysDown.down) {character.v[1] = speed;}
-      if (keysDown.up) {character.v[1] = -speed;}
-      if (keysDown.left) {character.v[0] = -speed;}
-      if (keysDown.right) {character.v[0] = speed;}
+    case "ArrowRight":
+    case "KeyD":
+      keysDown.right = true;
+      break;
 
-      character.p[0] += character.v[0];
-      character.p[1] += character.v[1];
+    case "ArrowLeft":
+    case "KeyA":
+      keysDown.left = true;
+      break;
+  }
+}
+addEventListener("keydown", recordKeyDown)
 
-      character.element.style.left = character.p[0] + 'px';
-      character.element.style.top = character.p[1] + 'px';
+function recordKeyUp(e) {
+    switch (e.code) {
+    case "ArrowDown":
+    case "KeyS":
+      keysDown.down = false;
+      break;
 
-      character.v = [0,0];
-    }
+    case "ArrowUp":
+    case "KeyW":
+      keysDown.up = false;
+      break;
 
-    let run = setInterval(draw, 1);
+    case "ArrowRight":
+    case "KeyD":
+      keysDown.right = false;
+      break;
+
+    case "ArrowLeft":
+    case "KeyA":
+      keysDown.left = false;
+      break;
+  }
+}
+addEventListener("keyup", recordKeyUp)
+
+let speed = 2;
+
+function draw() {
+    if (keysDown.down) {character.v[1] = speed;}
+    if (keysDown.up) {character.v[1] = -speed;}
+    if (keysDown.left) {character.v[0] = -speed;}
+    if (keysDown.right) {character.v[0] = speed;}
+
+    character.p[0] += character.v[0];
+    character.p[1] += character.v[1];
+
+    character.element.style.left = character.p[0] + 'px';
+    character.element.style.top = character.p[1] + 'px';
+
+    character.v = [0,0];
+}
+
+let run = setInterval(draw, 1);
